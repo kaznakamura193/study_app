7863bfded2fb888253e0f603b9adb651a5ccb66d
EC2 Default User <ec2-user@ip-172-31-44-82.us-east-2.compute.internal>
2018-07-03 13:42:52 +0000

    Finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 StudyApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :tssks
 
   # The priority is based upon order of creation: first created -> highest priority.
