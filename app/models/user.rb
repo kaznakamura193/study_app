01a8f3a4d2938569d3449af8531db83a9b7b5a52
EC2 Default User <ec2-user@ip-172-31-44-82.us-east-2.compute.internal>
2018-07-06 23:01:02 +0000

    Make a Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,25 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
         validates :name,  presence: true, length: { maximum: 50 }
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+       
+        
   def feed
     Micropost.where("user_id = ?", id)
   end
