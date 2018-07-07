82884b6fc9ed7876ac56ec802b3239034c673d27
EC2 Default User <ec2-user@ip-172-31-44-82.us-east-2.compute.internal>
2018-07-07 00:14:05 +0000

    Finish user follow / un follow actions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,15 @@
-class RegistrationsController < Devise::RegistrationsController
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
 
-  def after_sign_up_path_for(resource)
-    user_path(resource)
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    redirect_to @user
   end
- 
-  private
- 
-      def sign_up_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
- 
-      def account_update_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
-      end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    redirect_to @user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
         validates :name,  presence: true, length: { maximum: 50 }
-
+ 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
   end
@@ -18,9 +27,5 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
-       
-        
-  def feed
-    Micropost.where("user_id = ?", id)
-  end
+
 end
